489f0b82f2c3f554daa1c95212fdca2c9238aa31
rykwdaw-ui <rykwdaw@gmail.com>
2026-08-06 04:40:48 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA2PJTCkIl1DehcLLK_Wpy4X-QTVG4ycH0",
+  authDomain: "hmm-ecc74.firebaseapp.com",
+  projectId: "hmm-ecc74",
+  storageBucket: "hmm-ecc74.firebasestorage.app",
+  messagingSenderId: "1092745287735",
+  appId: "1:1092745287735:web:6e4dbce88f2d7117b507b2",
+  measurementId: "G-8V30XE656Y"
+};
